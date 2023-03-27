935ae393d8edaebd8d0c392441c5ae33b884e5bb
iamvirgoo <carens.kha4atryan@yandex.ru>
2023-03-27 19:20:07 +0300

    FIX: video player control buttons

--- app/src/pages/<player>/index.tsx
+++ app/src/pages/<player>/index.tsx
@@ -5,21 +5,24 @@ import styles from '../../styles/player.module.css'
 import play from '../../assets/player/play.svg';
 import pause from '../../assets/player/pause.svg';
 import revertImg from '../../assets/player/revert.svg';
-import forward from '../../assets/player/forward.svg';
+import forwardImg from '../../assets/player/forward.svg';
 import settings from '../../assets/player/settings.svg';
 import subtitles from '../../assets/player/subtitles.svg';
+import nonSubtitles from '../../assets/player/non-subtitles.svg';
 import full from '../../assets/player/full.svg';
+import collapseFull from '../../assets/player/collapse-full.svg'
 
 import film from '../../../../volumes/film/bunny.mp4'
-import {Simulate} from "react-dom/test-utils";
-import keyDown = Simulate.keyDown;
 
 const Player = (props : any) => {
     const videoRef = useRef(null);
     const [playing, setPlaying] = useState(false);
+    const [IsFullscreen, setIsFullscreen] = useState(false);
+    const [IsSubtitles, setIsSubtitles] = localStorage.getItem("disability") == "deafness" ?
+        useState(true) : useState(false);
     const [currentTime, setCurrentTime] = useState(0);
     const [videoTime, setVideoTime] = useState(0);
-    const [progress, setProgress] = useState(10);
+    const [progress, setProgress] = useState(0);
 
     const videoHandler = (control: string) => {
         if (control === "play") {
@@ -36,7 +39,7 @@ const Player = (props : any) => {
         }
     };
 
-    const fastForward = () => {
+    const forward = () => {
         // @ts-ignore
         videoRef.current.currentTime += 10;
     };
@@ -57,7 +60,6 @@ const Player = (props : any) => {
         <section className={styles.player}>
             <div className={styles.container}>
                 <h1 className={styles.title}>{props.movieTitle || "Movie Title"}</h1>
-                <p className={styles.description}>{props.movieDescription || "movie description movie description"}</p>
                 <div className={styles.videoContainer}>
                     <video
                         id="video1"
@@ -66,82 +68,82 @@ const Player = (props : any) => {
                         src={film}
                     ></video>
                     <div className={styles.timeControls}>
-                        <div className={styles.time_progressbarContainer}>
-                            <div
-                                style={{ width: `${progress}%` }}
-                                className={styles.time_progressBar}
-                            ></div>
-                            <p className={styles.controlsTime}>
-                                {Math.floor(currentTime / 60) +
-                                    ":" +
-                                    ("0" + Math.floor(currentTime % 60)).slice(-2)}
-                            </p>
+                        <div className={styles.barWrapper}>
+                            <div className={styles.progressContainer}>
+                                <div className={styles.progressBarContainer}>
+                                    <div style={{ width: `${progress}%` }} className={styles.time_progressBar}/>
+                                </div>
+                            </div>
+                            <div className={styles.time}>
+                                <p className={styles.controlsTime}>
+                                    {Math.floor(currentTime / 60) +
+                                        ":" +
+                                        ("0" + Math.floor(currentTime % 60)).slice(-2)
+                                    }
+                                </p>
+                            </div>
+                        </div>
+                        <div className={styles.controlsWrapper}>
+                            <div className={styles.leftControls}>
+                                {playing ? <div onClick={() => videoHandler("pause")} className={styles.button}>
+                                        <img
+                                            alt={''}
+                                            src={pause}
+                                        />
+                                    </div>
+                                    : <div onClick={() => videoHandler("play")} className={styles.button}>
+                                        <img
+                                            alt={''}
+                                            src={play}
+                                        />
+                                    </div>
+                                }
+                                <div onClick={revert} className={styles.button}>
+                                    <img
+                                        alt={''}
+                                        src={revertImg}
+                                    />
+                                </div>
+                                <div onClick={forward} className={styles.button}>
+                                    <img
+                                        alt={''}
+                                        src={forwardImg}
+                                    />
+                                </div>
+                            </div>
+                            <div className={styles.rightControls}>
+                                { IsSubtitles ? <div className={styles.button} onClick={() => setIsSubtitles(!IsSubtitles)}>
+                                    <img
+                                        alt={''}
+                                        src={subtitles}
+                                    />
+                                </div>
+                                    : <div className={styles.button} onClick={() => setIsSubtitles(!IsSubtitles)}>
+                                        <img
+                                            alt={''}
+                                            src={nonSubtitles}
+                                        />
+                                    </div>
+                                }
+                                <div className={styles.button}>
+                                    <img
+                                        alt={''}
+                                        src={settings}
+                                    />
+                                </div>
+                                <div className={styles.button}>
+                                    <img
+                                        alt={''}
+                                        src={full}
+                                    />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
     </main>
-
-    /*<div className={styles.app}>
-            <video
-                id="video1"
-                ref={videoRef}
-                className="video"
-                src={film}
-            ></video>
-
-            <div className={styles.controlsContainer}>
-                <div className={styles.controls}>
-                    <img
-                        onClick={revert}
-                        className={styles.controlsIcon}
-                        alt=""
-                        src={revertImg}
-                    />
-                    {playing ? (
-                        <img
-                            onClick={() => videoHandler("pause")}
-                            className={styles.controlsIconSmall}
-                            alt=""
-                            src={pause}
-                        />
-                    ) : (
-                        <img
-                            onClick={() => videoHandler("play")}
-                            className={styles.controlsIconSmall}
-                            alt=""
-                            src={play}
-                        />
-                    )}
-                    <img
-                        className={styles.controlsIcon}
-                        onClick={fastForward}
-                        alt=""
-                        src={forward}
-                    />
-                </div>
-            </div>
-
-            <div className={styles.timeControls}>
-                <p className={styles.controlsTime}>
-                    {Math.floor(currentTime / 60) +
-                        ":" +
-                        ("0" + Math.floor(currentTime % 60)).slice(-2)}
-                </p>
-                <div className={styles.time_progressbarContainer}>
-                    <div
-                        style={{ width: `${progress}%` }}
-                        className={styles.time_progressBar}
-                    ></div>
-                </div>
-                <p className={styles.controlsTime}>
-                    {Math.floor(videoTime / 60) +
-                        ":" +
-                        ("0" + Math.floor(videoTime % 60)).slice(-2)}
-                </p>
-            </div>
-        </div>*/
 }
 
 export default Player
